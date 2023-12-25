1c7a075924629c14ce3441c24ae6eb7d65fec74e
carlosefrain123 <carlosefrainchallo1@gmail.com>
2023-12-24 20:24:00 -0500

    24/12/2023 - 20:23 - Se ven las rutas de LARAVEL

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,39 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/**TODOS:Estas son las rutas, donde se definen */
+/* Route::get('/ruta', function () {
+    return "Hola soy una ruta";
+}); */
+/**TODO: Esta ruta recibe una ID */
+/* Route::get('/nombre/{nombre}', function ($nombre) {
+    return "Hola soy una ruta ".$nombre;
+}); */
+/**Solo permite en la URL palabras*/
+/* Route::get('/nombre/{name}', function (string $name) {
+    return "Hola soy una ruta ".$name;
+})->where('Hola soy ', '[A-Za-z]+'); */
+/**Solo permite en la URL números*/
+/* Route::get('/user/{id}', function (string $id) {
+    // ...
+})->where('id', '[0-9]+'); */
+/**Solo permite en la URL números y palabras*/
+/* Route::get('/user/{id}/{name}', function (string $id, string $name) {
+    // ...
+})->where(['Hola mi nombre es' => '[0-9]+', 'name' => '[a-z]+']); */
+
+/* //TODO: Se redirige
+Route::redirect('/publicaciones', 'http://localhost/laravelA/public/articulos');
+Route::get('articulos', function () {
+    return "Hola estoy en la ruta artículos";
+}); */
+
+/* ================== */
+//TODO: Se coloca nombre a la URL completa
+Route::get('/ruta', function () {
+    return "Hola soy una ruta";
+})->name('operacion');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
